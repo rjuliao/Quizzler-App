4fcdab169caf4497076a3f555bca21a463c0c4b7
Raul juliao <raulj@uninorte.edu.co>
2025-03-19 21:05:45 -0500

    PersonalizationData structure

---
+++ Quizzler-iOS13/Model/PersonalizationData.swift
@@ -0,0 +1,13 @@
+//
+//  PersonalizationData.swift
+//  Quizzler-iOS13
+//
+//  Created by Raul Humberto Juliao Colina on 19/03/25.
+//  Copyright © 2025 The App Brewery. All rights reserved.
+//
+
+struct PersonalizationData {
+    var accountId: String
+    var datasetIds: String
+
+}
